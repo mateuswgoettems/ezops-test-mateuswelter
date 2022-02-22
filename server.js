9ff016cf0817cf7219a5149cbfbb1379e4978c85
Mateus Welter <welter.mateus@gmail.com>
2022-02-21 22:56:47 -0300

    nova feature, hora mensagem

--- server.js
+++ server.js
@@ -1,71 +1,76 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-const mongoose = require("mongoose");
-const cors = require("cors");
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+const cors = require('cors');
 const corsOptions = {
-  origin: "*",
-};
+    origin: '*'
+}
 
 app.use(express.static(__dirname));
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-const Message = mongoose.model("Message", {
-  name: String,
-  message: String,
-});
+const Message = mongoose.model('Message', {
+    name: String,
+    message: String,
+    date: Date
+})
 
-const dbUrl =
-  "mongodb+srv://mateus:mateuswg123@cluster0.za1ho.mongodb.net/ezops?retryWrites=true&w=majority";
+const dbUrl = 'mongodb+srv://mateus:mateuswg123@cluster0.za1ho.mongodb.net/ezops?retryWrites=true&w=majority'
 
-app.get("/messages", (req, res) => {
-  Message.find({}, (err, messages) => {
-    res.send(messages);
-  });
-});
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
+})
 
-app.get("/messages/:user", (req, res) => {
-  var user = req.params.user;
-  Message.find({ name: user }, (err, messages) => {
-    res.send(messages);
-  });
-});
 
-app.post("/messages", async (req, res) => {
-  try {
-    const message = new Message(req.body);
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({ name: user }, (err, messages) => {
+        res.send(messages);
+    })
+})
 
-    await message.save();
-    console.log("saved");
 
-    const censored = await Message.findOne({ message: "badword" });
-    if (censored) await Message.remove({ _id: censored.id });
-    else io.emit("message", req.body);
-    res.sendStatus(200);
-  } catch (error) {
-    res.sendStatus(500);
-    return console.log("error", error);
-  } finally {
-    console.log("Message Posted");
-  }
-});
+app.post('/messages', async (req, res) => {
+    try {
+        const message = new Message(req.body);
 
-io.on("connection", () => {
-  console.log("a user is connected");
-});
+        await message.save()
+        console.log('saved');
 
-mongoose.connect(
-  dbUrl,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    console.log("mongodb connected", err);
-  }
-);
+        const censored = await Message.findOne({ message: 'badword' });
+        if (censored)
+            await Message.remove({ _id: censored.id })
+        else
+            io.emit('message', req.body);
+        res.sendStatus(200);
+    }
+    catch (error) {
+        res.sendStatus(500);
+        return console.log('error', error);
+    }
+    finally {
+        console.log('Message Posted')
+    }
+
+})
+
+
+
+io.on('connection', () => {
+    console.log('a user is connected')
+})
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    console.log('mongodb connected', err);
+})
 
 const server = http.listen(3000, () => {
-  console.log("server is running on port", server.address().port);
+    console.log('server is running on port', server.address().port);
 });
